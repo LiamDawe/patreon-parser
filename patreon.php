b39d5e15190b9da85f07157016ab762b31e232a6
Liam Dawe <liamdawe@gmail.com>
2017-01-12 20:34:08 +0000

    Update patreon.php

--- patreon.php
+++ patreon.php
@@ -12,5 +12,8 @@ foreach ($csv as $line)
 {
   // make it a proper decimal number to compare against
   $pledge = (float) $line[3];
+  
+  // do stuff here, like email them or something
+  // I check a database of existing users to match up an email and give them a special badge for supporting me
 }
 ?>
